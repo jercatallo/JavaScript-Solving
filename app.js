0b833bcc15b6806d336fc5d7bac98e0ad4f32281
Jer Carlo Catallo <49897728+rojerthat07@users.noreply.github.com>
2020-03-20 13:18:17 +0800

    new prob

--- app.js
+++ app.js
@@ -1,23 +1,12 @@
+//Persistent Bugger
+
 //Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question is still correct. However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
 //Your task is to write a function maskify, which changes all but the last four characters into '#'.
 
-//Problem Link - https://www.codewars.com/kata/5412509bd436bd33920011bc/train/javascript
+//Problem Link - https://www.codewars.com/kata/55bf01e5a717a0d57e0000ec/train/javascript
 
 
 
-function maskify(cc) {
-let mask= "";
-let j = cc;
-for(let i = 0; i <cc.length;i++){
-    if(i<cc.length -4){
-        mask += '#';
-    } else{
-        mask += j[i];
-    }
-
-}
-    console.log(mask);
-}
-
-
-maskify('4556364607935616');
+function persistence(num) {
+    //code me
+ }
