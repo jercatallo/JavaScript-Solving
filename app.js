20a057d765309912d03800964734602a9be4ab9d
Jer Carlo Catallo <49897728+rojerthat07@users.noreply.github.com>
2020-03-20 13:34:54 +0800

    borwnout

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-//Persistent Bugger
+//A square of squares
 
-//Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question is still correct. However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
-//Your task is to write a function maskify, which changes all but the last four characters into '#'.
+//You like building blocks. You especially like building blocks that are squares. And what you even like more, is to arrange them into a square of square building blocks!
+//However, sometimes, you can't arrange them into a square. Instead, you end up with an ordinary rectangle! Those blasted things! If you just had a way to know, whether you're currently working in vain… Wait! That's it! You just have to check if your number of building blocks is a perfect square.
 
-//Problem Link - https://www.codewars.com/kata/55bf01e5a717a0d57e0000ec/train/javascript
+//Problem Link - https://www.codewars.com/kata/54c27a33fb7da0db0100040e/train/javascript
 
 
 
-function persistence(num) {
-    //code me
- }
+var isSquare = function(n){
+    if(Math.sqrt(n) % 2 == 0){
+        return true;
+    } else{
+        return false
+    }
+  }
+
+
+  console.log(isSquare(26))
