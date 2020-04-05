3889e3cdfba54b8f9f646959fc75a22858f86b86
Jer Carlo Catallo <49897728+rojerthat07@users.noreply.github.com>
2020-04-05 19:32:33 +0800

    Simple Pig Latin
    //Problem Link - https://www.codewars.com/kata/520b9d2ad5c005041100000f/train/javascript

--- app.js
+++ app.js
@@ -1,117 +1,48 @@
-//Good vs Evil
+//Simple Pig Latin
 
-//Problem Link - https://www.codewars.com/kata/52761ee4cffbc69732000738/train/javascript
+//Problem Link - https://www.codewars.com/kata/520b9d2ad5c005041100000f/train/javascript
 
+function pigIt(str){
 
-//Good side:
-//Hobbits: 1
-//Men: 2
-//Elves: 3
-//Dwarves: 3
-//Eagles: 4
-//Wizards: 10
+  //Takes the Word
+  var word = str.split(" ");
+  //Piglatin Outcome
+  var pigLatin = [];
+  //Extension Word
+  const extensionWord = "ay";
 
-//On the side of evil we have:
-//Orcs: 1
-//Men: 2
-//Wargs: 2
-//Goblins: 2
-//Uruk Hai: 3
-//Trolls: 5
-//Wizards: 10
+  for(let i= 0;i <word.length;i++){
+    let newWord = "";
+    let placeholder = "";
 
+    for(let j =0; j <word[i].length ;j++){
+      //to takes the letters in the word except the first word
+      if(j != word[i].length - 1){
+        newWord += word[i][j + 1];
+      }
+      
 
-
-function goodVsEvil(good, evil){
-
-  let goods = good.split(" "), evils = evil.split(" ");
-  let goodCount = 0, evilCount = 0, result = "";
-  let  goodLength = good.length, evilLength = evil.length;
-
-
- 
-//Good
-// 0 1 2 3 4 5 
-// 1 2 3 3 4 10
-
-for(let i =0; i<goodLength;i++){
-  if(goods[i] >= 1){
-    switch(i){
-      case 0:
-      goodCount += 1 * goods[i];
-      break;
-      case 1:
-        goodCount += 2 * goods[i];
-      break;
-      case 2:
-        goodCount += 3 * goods[i];
-      break;
-      case 3:
-        goodCount += 3 * goods[i];
-      break;
-      case 4:
-        goodCount += 4 * goods[i];
-      break;
-      case 5:
-        goodCount += 10 * goods[i];
-      break;
+      if(j == word[i].length - 1){
+        //to take the first letter of the word in a array
+        placeholder += word[i][0];
+        //Leave punctuation marks untouched
+        if(word[i][j] != "!" && word[i][j] != "?"){
+          //to add letters in the word except the first word + first letter of the word in a array + "ay"
+          pigLatin.push(newWord + placeholder + extensionWord) 
+        }else{
+          //to add letters in the word except the first word + first letter of the word in a array
+          pigLatin.push(newWord + placeholder) 
+        }
+      }
     }
-   
-  } else{
-    continue;
   }
 
-}
-
-//Evil
-// 0 1 2 3 4 5 6
-// 1 2 2 2 3 5 10
-
-for(let i =0; i<evilLength;i++){
-
-  if(evils[i] >= 1){
-    switch(i){
-      case 0:
-      evilCount += 1 * evils[i];
-      break;
-      case 1:
-      evilCount += 2 * evils[i];
-      break;
-      case 2:
-      evilCount += 2* evils[i];
-      break;
-      case 3:
-      evilCount += 2 * evils[i];
-      break;
-      case 4:
-      evilCount += 3 * evils[i];
-      break;
-      case 5:
-      evilCount += 5 * evils[i];
-      break;
-      case 6:
-      evilCount += 10 * evils[i];
-      break;
-    }
-   
-  } else{
-    continue;
-  }
-
-}
-
-if(goodCount > evilCount){
-  result += "Battle Result: Good triumphs over Evil";
-} else if(evilCount > goodCount){
-  result += "Battle Result: Evil eradicates all trace of Good" ;
-}else if(goodCount === evilCount) {
-  result += "Battle Result: No victor on this battle field";
-}
-
-console.log(goodCount);
-console.log(evilCount);
+  //to converts piglatin to full string
+  var final = pigLatin.join(" ");
+  
+  return final;
 
 }
 
 
-goodVsEvil("1 1 1 1 1 1", "1 1 1 1 1 1 1");
+pigIt('This is my string !')
