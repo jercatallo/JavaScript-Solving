6c48634c56d707bd80a690fd27d14cabc09f1ead
Jer Carlo Catallo <49897728+rojerthat07@users.noreply.github.com>
2020-04-25 16:11:59 +0800

    Data Structure
    Find pair with given sum in the array

--- app.js
+++ app.js
@@ -1,53 +1,33 @@
-// function palindromes(string){
-
-//   var newWord = "";
-//   var palindrome = [];
-//   var reversePalindrome = [];
-//   var allPalindrome = [];
-
-
-//   for(let i = 0;i<string.length;i++){
-//     newWord += string[i]
-//     palindrome.push(newWord);
-//   }
-
-
-//   for(let i = 0; i<palindrome.length;i++){
-//     var reverseWord = "";
-//     var reverse = "";
-
-//     for(let j = 0; j < i +1;j++){
-//         //to get the word in array element
-//         reverseWord += palindrome[i][j];
-//     }
-
-    
-//     for(let z = reverseWord.length -1; z >=0; z--){
-//       //to reverse the wordin array element
-//       reverse += reverseWord[z];
-//     }
-
-//     //to push the reverse word to new array
-//    reversePalindrome.push(reverse);
-
-
-//    //to set to default the word getters
-//     reverse = "";
-//     reverseWord = "";
-//   }
-
-//   for(let i = 1; i<palindrome.length;i++){
-//     if(palindrome[i] == reversePalindrome[i]){
-//       allPalindrome.push(palindrome[i]);
-//     }
-//   }
-
-//   document.write(allPalindrome)
-//   console.log(palindrome)
-//   console.log(reversePalindrome)
-// }
+//Data Structure
+//Find pair with given sum in the array
 
 
 
+var arr = [8,7,2,5,3,1];
+var sum = 10;
 
-// palindromes("abaaabaa ");
+//Set the checker to false
+var checkPair = false;
+
+
+for(let i =0; i<arr.length;i++){
+
+
+    for(let j =0; j<arr.length/2;j++){
+        //To not include the same integer itself in the process
+        if(i != j){
+             //Takes all the integer starting from i = 0 to j = 0...5 and adds it
+        if(arr[i] + arr[j] == sum){
+            //Display the pair that equals to sum
+            console.log("Pair found at index " + i + " and " + j + "("+arr[i] +" + " +arr[j]+")");
+            //Changing the value of checkPair to true because it passes the conditional statement above that checks if the integer[i] + integer[j] == sum
+            checkPair = true;
+        }
+    }
+    }
+}
+
+//Display if their is no pair
+if(checkPair == false){
+    console.log("There is no pair");
+}
