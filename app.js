27fc29c254fe914b8fa2617d74b7db014a152327
Jer Carlo Catallo <49897728+rojerthat07@users.noreply.github.com>
2020-06-11 09:14:00 +0800

    yes

--- app.js
+++ app.js
@@ -1,33 +1,23 @@
-//Data Structure
-//Find pair with given sum in the array
+let input = "<hr><br>Save<br>";
+
+let hr ="hr";
+let br ="br";
 
 
 
-var arr = [8,7,2,5,3,1];
-var sum = 10;
+for(let i = 0; i< input.length;i++){
+    for(let j = 0;j< 4;j++){
+        let placeholder = "";
+        placeholder += input[j]
+       console.log(placeholder)
+        if( placeholder != "<hr>" || placeholder != "<br>"){
 
-//Set the checker to false
-var checkPair = false;
-
-
-for(let i =0; i<arr.length;i++){
-
-
-    for(let j =0; j<arr.length/2;j++){
-        //To not include the same integer itself in the process
-        if(i != j){
-             //Takes all the integer starting from i = 0 to j = 0...5 and adds it
-        if(arr[i] + arr[j] == sum){
-            //Display the pair that equals to sum
-            console.log("Pair found at index " + i + " and " + j + "("+arr[i] +" + " +arr[j]+")");
-            //Changing the value of checkPair to true because it passes the conditional statement above that checks if the integer[i] + integer[j] == sum
-            checkPair = true;
+            placeholder = "";
         }
-    }
+       
     }
 }
 
-//Display if their is no pair
-if(checkPair == false){
-    console.log("There is no pair");
-}
+
+
+
