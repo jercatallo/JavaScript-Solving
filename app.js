392336758fa314808a38052e85341d952a3a0dcc
Jer Carlo Catallo <49897728+rojerthat07@users.noreply.github.com>
2020-06-17 11:10:02 +0800

    The Vowel Code
    https://www.codewars.com/kata/53697be005f803751e0015aa/train/javascript

--- app.js
+++ app.js
@@ -1,15 +1,60 @@
-function largestPairSum(numbers)
-{
-  var num = numbers.sort(function(a,b){return a-b});
-  console.log(num)
- 
-    console.log(num[num.length-1] + num[num.length-2])
+function encode(string) {
+  var encoded = "";
+  for(let i =0; i <string.length;i++){
+    switch(string[i]){
+      case 'a' || 'A':
+        encoded += '1';
+        break;
+        case 'e' || 'E':
+          encoded += '2';
+          break;
+          case 'i' || 'I':
+            encoded += '3';
+            break;
+            case 'o' || 'O':
+              encoded += '4';
+              break;
+              case 'u' || 'U':
+                encoded += '5';
+                break;
+                default:
+                  encoded += string[i];
+    }
+  }
+  console.log(encoded)
   
 
+}
 
+function decode(string) {
+  var decoded = "";
+  for(let i =0; i <string.length ;i++){
+    switch(string[i]){
+      case '1':
+        decoded += 'a';
+        break;
+        case '2':
+          decoded += 'e';
+          break;
+          case '3':
+            decoded += 'i';
+            break;
+            case '4':
+              decoded += 'o';
+              break;
+              case '5':
+                decoded += 'u';
+                break;
+                default:
+                  decoded += string[i];
+    }
+  }
+  console.log(decoded)
 }
 
 
 
 
-largestPairSum([-10, -14, -2, -23, -19])
+
+encode("hello");
+decode("h2ll4")
