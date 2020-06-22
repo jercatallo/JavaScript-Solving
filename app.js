a5bd96c69bd464f821a3a90c56a66f9a21d7dd8d
Jer Carlo Catallo <49897728+rojerthat07@users.noreply.github.com>
2020-06-22 08:00:28 +0800

    Series of integers from m to n
    https://www.codewars.com/kata/5841f680c5c9b092950001ae/train/javascript

--- app.js
+++ app.js
@@ -1,30 +1,10 @@
-function capitalize(s){
-var capitalize = [];
-var evenCap = "";
-var oddCap = "";
-
-  for(let i =0;i<s.length;i++){
-    if(i % 2 == 0){
-      evenCap += s[i].toUpperCase();
-    }else{
-      evenCap += s[i];
-    }
+function generateIntegers(m, n) {
+  for(var i = m;i <= n;i++){
+    output += i;
   }
-
-  for(let i =0;i<s.length;i++){
-    if(i % 2 >= 1){
-      oddCap += s[i].toUpperCase();
-    }else{
-      oddCap += s[i];
-    }
-  }
-
-  capitalize.push(evenCap,oddCap);
+  var output;
+  return output;
+}
 
 
-   return capitalize;
-};
-
-
-
-capitalize("abcdef");
+generateIntegers(2,5);
