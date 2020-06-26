ab58d4de051a58be17c7a6fc6e0b02aa5bb24f7e
Jer Carlo Catallo <jercarlopogi07@gmail.com>
2020-06-26 16:15:21 +0800

    Palindrome Number
    https://leetcode.com/problems/palindrome-number/

--- app.js
+++ app.js
@@ -1,37 +1,12 @@
-var addTwoNumbers = function(l1, l2) {
-    
-  var num1 = "";
-  var num2 = "";
-
-  
-
-  for(let i = l1.length -1; i >=0;i--){
-    num1 += l1[i];
+var isPalindrome = function(x) {
+  var x = x.toString(),z= "";
+  for(let i = x.length - 1;i>=0;i--){
+    z += x[i]
   }
-
-  for(let i = l2.length -1; i >=0;i--){
-    num2 += l2[i];
-  }
-
-  var output = Number(num1) + Number(num2) + "";
-  var final = [];
-
- 
-  for(let i = output.length -1; i >=0;i--){
-  
-
-    final.push(Number(output[i]));
-  }
-  
-
-  return final;
-  
+  return z == x ?  true : false
 };
 
 
 
 
-
-
-
-addTwoNumbers([2,4,3],[5,6,4])
+isPalindrome(1331);
